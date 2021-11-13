fc84cb4ef831b5ea1a630fc733de4df31e3c15df
prakritj <p.jayakumar1@lse.ac.uk>
2021-11-13 14:22:19 +0000

    added comments

--- PRAW_trial.py
+++ PRAW_trial.py
@@ -9,21 +9,30 @@ reddit = praw.Reddit(
     username="ds105_WSB",
 )
 
-post_df = pd.DataFrame()
+posts_df = pd.DataFrame()
 
 wsb = reddit.subreddit('wallstreetbets')
 for submission in wsb.search('Daily Discussion Thread for', sort='new', time_filter='all', limit=500):
     if 'Daily Discussion Thread for' in submission.title:
         if 'June' in submission.title or 'July' in submission.title:
-            #I need title, id, comments, score, put into post_df
-            post_df = post_df.append({
+            #I need title, id, comments, score, put into posts_df
+            posts_df = posts_df.append({
                 'id':submission.id,
                 'title':submission.title,
                 'score':submission.score,
-                'comments':submission.comments
+                'comments':submission.comments.replace_more(limit=None)
             }, ignore_index=True)
 
-print(post_df)
+posts_df.to_csv(r'posts_df.csv')
 
-for comments in post_df:
-    
+comments_df = pd.DataFrame()
+for comments in posts_df:
+    for top_level_comments in comments:
+        comments_df = comments_df.append({
+            'id':comment.id,
+            'post_id':comment.link_id,
+            'body':comment.body,
+            'score':comment.score
+        }, ignore_index=True)
+
+comments_df.to_csv(r'posts_df.csv')
