24bae70ab79d4fef368b6b0a990a1eabd3951e36
prakritj <p.jayakumar1@lse.ac.uk>
2021-11-13 12:28:25 +0000

    added dataframe

--- PRAW_trial.py
+++ PRAW_trial.py
@@ -9,10 +9,18 @@ reddit = praw.Reddit(
     username="ds105_WSB",
 )
 
-df = pd.DataFrame()
+post_df = pd.DataFrame()
 
 wsb = reddit.subreddit('wallstreetbets')
 for submission in wsb.search('Daily Discussion Thread for', sort='new', time_filter='all', limit=500):
     if 'Daily Discussion Thread for' in submission.title:
         if 'June' in submission.title or 'July' in submission.title:
-            print(submission.title)
+            #I need title, id, comments, score, put into post_df
+            post_df = post_df.append({
+                'id':submission.id,
+                'title':submission.title,
+                'score':submission.score,
+                'comments':submission.comments
+            }, ignore_index=True)
+
+print(post_df)
